bc4f969f341c37a5a85f7bfea0708dc9a79cab5c
nalinisaxena <nalinisaxena13@gmail.com>
2016-10-13 14:55:14 +0530

    first program

---
+++ VarDemo.java
@@ -0,0 +1,16 @@
+class VarDemo
+{
+public static void main(String args[])
+{
+int a;
+float b;
+String s;
+a=10;
+b=3.14f;
+s="Spark";
+
+System.out.println("int "+a);
+System.out.println("float "+b);
+System.out.println("string "+s);
+}
+}
